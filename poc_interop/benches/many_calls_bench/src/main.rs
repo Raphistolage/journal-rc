ba007409b0487ead55bd5916cd200b838868db7e
Raphistolage <clissonr@UN00317976-UNAL.dam.intra.cea.fr>
2025-12-08 11:04:24 +0100

    Début ajout bench pour comparer perfs

---
+++ poc_interop/benches/many_calls_bench/src/main.rs
@@ -0,0 +1,62 @@
+
+use poc_interop::{Dim1, Dim2, DeviceSpace, LayoutRight, RustView, y_ax_device, kokkos_finalize, kokkos_initialize};
+
+use std::time::Instant;
+use ndarray::{ArrayView, ArrayView1, ArrayView2};
+
+fn y_ax_rust(y: &ArrayView1<f64>, a: &ArrayView2<f64>, x: &ArrayView1<f64>) -> f64 {
+    let mut result = 0.0;
+    for j in 0..a.shape()[0] {
+        let mut temp = 0.0;
+        for i in 0..a.shape()[1] {
+            temp += a[[j,i]]*x[[i]];
+        }
+        result += temp*y[[j]];
+    }
+    return result
+}
+
+
+fn main() {
+    println!("Many calls with pure rust for y_ax operation.");
+    let timer = Instant::now();
+    const N: i32 = 5_000_000;
+    for i in 0..N
+    {   
+        let data1 = [2.0+(i as f64), 2.0+(i as f64), 3.0+(i as f64), 4.0+(i as f64), 5.0+(i as f64)];
+        let y = ArrayView::from_shape((5), &data1).unwrap();
+        let data2 = [3.0+(i as f64), 2.0+(i as f64), 3.0+(i as f64), 4.0+(i as f64), 5.0+(i as f64), 6.0+(i as f64), 7.0+(i as f64), 8.0+(i as f64), 9.0+(i as f64), 10.0+(i as f64)];
+        let a = ArrayView::from_shape((5,2), &data2).unwrap();
+        let data3 = [4.0+(i as f64), 2.0+(i as f64)];
+        let x = ArrayView::from_shape((2), &data3).unwrap();
+        let result = y_ax_rust(&y, &a, &x);
+    }
+
+    let total_time = timer.elapsed().as_secs_f64();
+    println!("Total elapsed time with pure Rust : {}", total_time);
+    println!("Time per iteration : {}", total_time/(N as f64));
+
+
+    println!("Many calls with Kokkos kernel.");
+    let timer = Instant::now();
+
+    kokkos_initialize();
+    for i in 0..N
+    {   
+        let mut data1 = [2.0+(i as f64), 2.0+(i as f64), 3.0+(i as f64), 4.0+(i as f64), 5.0+(i as f64)];
+        let y = RustView::<'_, f64, Dim1, DeviceSpace, LayoutRight>::from_shape(&[5], &mut data1);
+
+        let mut data2 = [3.0+(i as f64), 2.0+(i as f64), 3.0+(i as f64), 4.0+(i as f64), 5.0+(i as f64), 6.0+(i as f64), 7.0+(i as f64), 8.0+(i as f64), 9.0+(i as f64), 10.0+(i as f64)];
+        let a = RustView::<'_, f64, Dim2, DeviceSpace, LayoutRight>::from_shape(&[5, 2], &mut data2);
+
+        let mut data3 = [4.0+(i as f64), 2.0+(i as f64)];
+        let x = RustView::<'_, f64, Dim1, DeviceSpace, LayoutRight>::from_shape(&[2], &mut data3);
+
+        let _ = y_ax_device(&y, &a, &x);
+    }
+    kokkos_finalize();
+
+    let total_time = timer.elapsed().as_secs_f64();
+    println!("Total elapsed time with Kokkos kernel : {}", total_time);
+    println!("Time per iteration : {}", total_time/(N as f64));
+}
--- poc_interop/src/cpp/rust_view.cpp
+++ poc_interop/src/cpp/rust_view.cpp
@@ -112,6 +112,44 @@ namespace rust_view {
         return result;
     }
 
+    void many_y_ax_device(const OpaqueView& y, const OpaqueView& A, const OpaqueView& x, int N) {
+        if (y.rank != 1 || A.rank != 2 || x.rank != 1) {
+            std::cout << "Ranks : y : " << y.rank << " A: " << A.rank << " x: " << x.rank <<" \n";
+            throw std::runtime_error("Bad ranks of views.");
+        } else if (A.shape[1] != x.shape[0] || A.shape[0] != y.shape[0]) {
+            throw std::runtime_error("Incompatible shapes.");
+        }
+
+        auto* y_view_ptr = static_cast<Kokkos::View<double*, Kokkos::LayoutRight, DeviceMemorySpace>*>(y.view->get_view());
+        auto* a_view_ptr = static_cast<Kokkos::View<double**, Kokkos::LayoutRight, DeviceMemorySpace>*>(A.view->get_view());
+        auto* x_view_ptr = static_cast<Kokkos::View<double*, Kokkos::LayoutRight, DeviceMemorySpace>*>(x.view->get_view());
+
+        int N = A.shape[0];
+        int M = A.shape[1];
+
+        auto y_view = *y_view_ptr;
+        auto a_view = *a_view_ptr;
+        auto x_view = *x_view_ptr;
+        
+        for (size_t i = 0; i < N; i++)
+        {
+            double result = 0;
+
+            Kokkos::parallel_reduce( N, KOKKOS_LAMBDA ( const int j, double &update ) {
+                double temp2 = 0;
+
+                for ( int i = 0; i < M; ++i ) {
+                    
+                    temp2 += a_view( j, i ) * x_view( i );
+                }
+
+                update += y_view( j ) * temp2;
+            }, result );
+
+            Kokkos::fence();
+        }
+    }
+
     void matrix_product(const OpaqueView& A, const OpaqueView& B, OpaqueView& C) {
         if (A.rank != 2 || B.rank != 2 || C.rank != 2) {
             std::cout << "Ranks : B : " << B.rank << " A: " << A.rank <<" \n";
