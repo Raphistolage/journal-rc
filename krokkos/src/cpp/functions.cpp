828a24e4ec5cdb39e144aa97d2beb3293b341e7f
Raphistolage <clissonr@UN00317976-UNAL.dam.intra.cea.fr>
2025-12-12 14:41:41 +0100

    Clean du functions.cpp

--- krokkos/src/cpp/functions.cpp
+++ krokkos/src/cpp/functions.cpp
@@ -1,12 +1,12 @@
 #include "functions.hpp"
 
-extern "C" {
-    SharedArray view_to_shared_c(const OpaqueView* opaqueView) {
-        return opaqueView->view->view_to_shared();  
-    }
+// extern "C" {
+//     SharedArray view_to_shared_c(const OpaqueView* opaqueView) {
+//         return opaqueView->view->view_to_shared();  
+//     }
 
-    SharedArrayMut view_to_shared_mut_c(const OpaqueView* opaqueView) {
-        return opaqueView->view->view_to_shared_mut();  
-    }
-}
+//     SharedArrayMut view_to_shared_mut_c(const OpaqueView* opaqueView) {
+//         return opaqueView->view->view_to_shared_mut();  
+//     }
+// }
 
