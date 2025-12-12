f11e742e5d248235687245c2e1d884dcd3646191
Raphistolage <clissonr@UN00317976-UNAL.dam.intra.cea.fr>
2025-12-12 14:22:52 +0100

    Modification de la templated macro pour inclure les structs dans le bridge

--- templated/templated_macro/src/lib.rs
+++ templated/templated_macro/src/lib.rs
@@ -5,7 +5,7 @@ use syn::{ItemMod, parse_macro_input};
 #[proc_macro_attribute]
 pub fn templated(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let module = parse_macro_input!(item as ItemMod);
-    let file_name = format!("/{}_ffi.rs", module.ident.to_string());
+    let file_name = format!("{}_ffi.rs", module.ident.to_string());
     let file_path = format!("{}/{}",std::env::var("OUT_DIR").expect("OUT_DIR not defined"), file_name);
     println!("LE file path est : {}", file_path);
     quote! {
