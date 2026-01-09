ff2b264c28968af4667487cee9d1827ca45971c1
Raphistolage <raphael.clisson@cea.fr>
2026-01-09 08:43:03 +0100

    m

--- proto_vec/proto_vec/src/main.rs
+++ proto_vec/proto_vec/src/main.rs
@@ -1,7 +1,9 @@
 mod ffi;
 
+use ffi::ProtoVector;
+
 fn main() {
-    let v = ffi::ProtoVector::<f64>::from_slice(&[1.0, 2.0, 3.0]);
+    let v = ProtoVector::<f64>::from_slice(&[1.0, 2.0, 3.0]);
     let a = v.at(2);
     println!("Value is : {:?}", a);
 
