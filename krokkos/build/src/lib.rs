d08b18343369aa1bb6bb29157d991e3fff07d14e
Raphistolage <raphael.clisson@cea.fr>
2026-01-09 15:33:17 +0100

    fix du cmake et du build script du module korkkos/build pour pouvoir ajouter ses propres fichiers et les faires tourner avec krokkos

--- krokkos/build/src/lib.rs
+++ krokkos/build/src/lib.rs
@@ -1,36 +1,56 @@
 use cmake::Config;
 use std::path::Path;
+use std::env;
 
 const KROKKOS_CRATE_ROOT: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/..");
 
 pub fn bridge(rust_source_file: Option<impl AsRef<Path>>, cpp_source_file: Option<impl AsRef<Path>>){
 
+    let user_manifest_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+
     let mut dst_config = Config::new(format!("{}/Release", KROKKOS_CRATE_ROOT));
 
-    let shared_dir = format!("{}/../../../../", std::env::var("OUT_DIR").expect("OUT_DIR not defined"));
+    let out_dir = std::env::var("OUT_DIR").expect("OUT_DIR not defined");
+    println!("cargo:warning=Out_dir is : {}", out_dir);
 
-    if !std::fs::exists(format!("{}krokkosbridge", shared_dir)).unwrap() {
-        std::fs::create_dir(format!("{}/../../../../krokkosbridge", std::env::var("OUT_DIR").expect("OUT_DIR not defined"))).unwrap();
+    if !std::fs::exists(format!("{}/../../../../krokkosbridge", out_dir)).unwrap() {
+        println!("cargo:warning=Creating krokkosbridge folder");
+        std::fs::create_dir(format!("{}/../../../../krokkosbridge", out_dir)).unwrap();
     }
+
+    let cpp_source_file_path = cpp_source_file.unwrap();
+    let cpp_source_file_path = cpp_source_file_path.as_ref();
+    let cpp_source_folder_str = cpp_source_file_path.parent().unwrap();
+    let cpp_source_file_str = cpp_source_file_path.to_str().unwrap();
 
     let modified_dst_config = dst_config
         .configure_arg("-DCMAKE_BUILD_TYPE=Release")
         .configure_arg(format!(
             "-DOUT_DIR={}",
-            shared_dir  
+            out_dir
+        ))
+        .configure_arg(format!(
+            "-DUSER_MANIFEST_DIR={}",
+            user_manifest_dir
+        ))
+        .configure_arg(format!(
+            "-DCPP_FOLDER={}",
+            cpp_source_folder_str.display()
         ))
         .configure_arg(format!(
             "-DPKG_NAME={}",
             std::env::var("CARGO_PKG_NAME").expect("PKG_NAME is not defined")
-        ));
+        ))
+    ;
 
-    if rust_source_file.is_some() {
+    let is_rust_source_file = rust_source_file.is_some();
+    if  is_rust_source_file {
         let path = rust_source_file.unwrap();
         let _ = cxx_build::bridge(&path);
 
         modified_dst_config.configure_arg(format!(
             "-DUSER_CPP_FILE_PATH={}",
-            cpp_source_file.unwrap().as_ref().to_str().unwrap()
+            cpp_source_file_str
         ))
         .configure_arg(format!(
             "-DUSER_RUST_FFI_FILE_PATH={}",
@@ -52,8 +72,10 @@ pub fn bridge(rust_source_file: Option<impl AsRef<Path>>, cpp_source_file: Optio
     println!("cargo:rustc-link-search=native={}", dst.display());
     println!("cargo:rustc-link-lib=rustViewFunctionsFfi");
     println!("cargo:rustc-link-lib=rustView");
-    println!("cargo:rustc-link-lib=sharedRustViewFfiTypes");
-    println!("cargo:rustc-link-lib=kokkosHandles");
+    println!("cargo:rustc-link-lib=rustViewFfiTypes");
+    if is_rust_source_file {
+        println!("cargo:rustc-link-lib=userCppFunctions")
+    }
     println!("cargo:rustc-link-arg=-Wl,-rpath={}", dst.display());
 
     // Only rerun build script when these files change
