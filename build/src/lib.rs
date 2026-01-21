b320cceba7145c8151b8372568c123a2f254d97c
Raphistolage <raphael.clisson@telecom-paris.fr>
2026-01-21 12:34:43 +0100

    Modif out_dir en outdir/../../../.. pour éviter répétitions.

--- build/src/lib.rs
+++ build/src/lib.rs
@@ -9,12 +9,13 @@ const KROKKOS_CRATE_ROOT: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/..");
 pub fn build(rust_source_file: impl AsRef<std::path::Path>){
     krokkos_gen::bridge(rust_source_file);
 
-    let out_dir = std::env::var("OUT_DIR").expect("OUT_DIR not defined");
+    let mut out_dir = std::env::var("OUT_DIR").expect("OUT_DIR not defined");
+    out_dir.push_str("/../../../..");
     println!("cargo:warning=Out_dir : {}", out_dir);
 
-    if !std::fs::exists(format!("{}/../../../../krokkosbridge", out_dir)).unwrap() {
+    if !std::fs::exists(format!("{}/krokkosbridge", out_dir)).unwrap() {
         println!("cargo:warning=Creating krokkosbridge folder");
-        std::fs::create_dir(format!("{}/../../../../krokkosbridge", out_dir)).unwrap();
+        std::fs::create_dir(format!("{}/krokkosbridge", out_dir)).unwrap();
     }
 
     let mut dst_config = Config::new(format!("{}/build/Release", KROKKOS_CRATE_ROOT));
