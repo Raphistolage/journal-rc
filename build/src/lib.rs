2fa8eab668f0ecfb8939bf26526c1b98fd0087d8
Raphistolage <--add>
2026-01-23 13:34:46 +0100

    Format all.

--- build/src/lib.rs
+++ build/src/lib.rs
@@ -5,20 +5,23 @@ use std::path::Path;
 const KROKKOS_CRATE_ROOT: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/..");
 
 /// Build function to generate and link the Views and their associated functions.
-/// 
+///
 /// Calls the Krokkos bridge generator, based on the views configurations specified in the `krokkos_init_configs` arguments found in the provided Rust source file,
 /// and proceeds to build, compile and link to it, as well as Kokkos, using CMake.
-/// 
+///
 /// This is a function to be called from a build script (`build.rs`).
-/// 
+///
 /// ***rust_source_file*** : The path to the rust file calling the `krokkos_init_configs` macro. It is recommended to do it in a dedicated rust file.
-pub fn build(rust_source_file: impl AsRef<Path>){
+pub fn build(rust_source_file: impl AsRef<Path>) {
     krokkos_gen::bridge(rust_source_file);
 
     let out_dir = env::var("OUT_DIR").expect("OUT_DIR not defined");
 
     let out_path = Path::new(&out_dir);
-    let target_path = out_path.ancestors().nth(4).expect("Cannot reach the target folder.");
+    let target_path = out_path
+        .ancestors()
+        .nth(4)
+        .expect("Cannot reach the target folder.");
 
     // We consider that our crate Krokkos is being used directly by the user, not through another crate.
     // This assumption makes it easy to move from the out_dir to the target_dir, following ../../../../
@@ -29,16 +32,14 @@ pub fn build(rust_source_file: impl AsRef<Path>){
 
     if !target_path.join("krokkosbridge").exists() {
         println!("cargo:warning=Creating krokkosbridge folder");
-        std::fs::create_dir(target_path.join("krokkosbridge")).expect("Error when creating the krokkosbridge folder.");
+        std::fs::create_dir(target_path.join("krokkosbridge"))
+            .expect("Error when creating the krokkosbridge folder.");
     }
 
     let mut dst_config = Config::new(format!("{}/build/cmake", KROKKOS_CRATE_ROOT));
     let modified_dst_config = dst_config
         .configure_arg("-DCMAKE_BUILD_TYPE=Release")
-        .configure_arg(format!(
-            "-DTARGET_DIR={}",
-            target_path.display()  
-        ))
+        .configure_arg(format!("-DTARGET_DIR={}", target_path.display()))
         .configure_arg(format!(
             "-DPKG_NAME={}",
             env::var("CARGO_PKG_NAME").expect("PKG_NAME is not defined")
