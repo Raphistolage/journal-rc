344bc82fedaa2b0fc9752aa193b6ecf327616afc
Raphistolage <--add>
2026-01-23 11:31:40 +0100

    Handle du target_path comme un std::path::Path. Plus propre.

--- build/src/lib.rs
+++ build/src/lib.rs
@@ -15,25 +15,30 @@ const KROKKOS_CRATE_ROOT: &str = concat!(env!("CARGO_MANIFEST_DIR"), "/..");
 pub fn build(rust_source_file: impl AsRef<Path>){
     krokkos_gen::bridge(rust_source_file);
 
-    let mut target_dir = env::var("OUT_DIR").expect("OUT_DIR not defined");
-    target_dir.push_str("/../../../..");
+    let out_dir = env::var("OUT_DIR").expect("OUT_DIR not defined");
+
+    let out_path = Path::new(&out_dir);
+    let target_path = out_path.ancestors().nth(4).expect("Cannot reach the target folder.");
 
     // We consider that our crate Krokkos is being used directly by the user, not through another crate.
     // This assumption makes it easy to move from the out_dir to the target_dir, following ../../../../
     // If we want to cover transitive dependencies (where Krokkos is being used through another crate, or more), we would need to implement the same method as Cxx,
     // Which would be to navigate in the parent folders, starting from OUT_DIR, until finding a .rustc_info.json file, meaning the current folder is the target folder.
 
-    println!("cargo:warning=TARGET_DIR : {}", target_dir);
+    println!("cargo:warning=TARGET_DIR : {}", target_path.display());
 
-    if !std::fs::exists(format!("{}/krokkosbridge", target_dir)).expect("Cannot create the krokkosbridge folder.") {
+    if !target_path.join("krokkosbridge").exists() {
         println!("cargo:warning=Creating krokkosbridge folder");
-        std::fs::create_dir(format!("{}/krokkosbridge", target_dir)).expect("Error when creating the krokkosbridge folder.");
+        std::fs::create_dir(target_path.join("krokkosbridge")).expect("Error when creating the krokkosbridge folder.");
     }
 
     let mut dst_config = Config::new(format!("{}/build/cmake", KROKKOS_CRATE_ROOT));
     let modified_dst_config = dst_config
         .configure_arg("-DCMAKE_BUILD_TYPE=Release")
-        .configure_arg(format!("-DTARGET_DIR={}", target_dir))
+        .configure_arg(format!(
+            "-DTARGET_DIR={}",
+            target_path.display()  
+        ))
         .configure_arg(format!(
             "-DPKG_NAME={}",
             env::var("CARGO_PKG_NAME").expect("PKG_NAME is not defined")
