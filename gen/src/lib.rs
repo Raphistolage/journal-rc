341713abc5c55d2044f5d2994b3b5e59a58df471
Raphistolage <--add>
2026-01-26 11:39:00 +0100

    Format + clippy

--- gen/src/lib.rs
+++ gen/src/lib.rs
@@ -164,18 +164,30 @@ inline void kokkos_finalize() {{
                     let fn_deep_copy_htd_ident = format_ident!("deep_copy_htd_{}", raw_extension);
                     let fn_deep_copy_dth_ident = format_ident!("deep_copy_dth_{}", raw_extension);
                     let fn_deep_copy_dtd_ident = format_ident!("deep_copy_dtd_{}", raw_extension);
-                    let fn_create_mirror_hth_ident = format_ident!("create_mirror_hth_{}", raw_extension);
-                    let fn_create_mirror_dth_ident = format_ident!("create_mirror_dth_{}", raw_extension);
-                    let fn_create_mirror_htd_ident = format_ident!("create_mirror_htd_{}", raw_extension);
-                    let fn_create_mirror_dtd_ident = format_ident!("create_mirror_dtd_{}", raw_extension);
-                    let fn_create_mirror_view_hth_ident = format_ident!("create_mirror_view_hth_{}", raw_extension);
-                    let fn_create_mirror_view_dth_ident = format_ident!("create_mirror_view_dth_{}", raw_extension);
-                    let fn_create_mirror_view_htd_ident = format_ident!("create_mirror_view_htd_{}", raw_extension);
-                    let fn_create_mirror_view_dtd_ident = format_ident!("create_mirror_view_dtd_{}", raw_extension);
-                    let fn_create_mirror_view_and_copy_hth_ident = format_ident!("create_mirror_view_and_copy_hth_{}", raw_extension);
-                    let fn_create_mirror_view_and_copy_dth_ident = format_ident!("create_mirror_view_and_copy_dth_{}", raw_extension);
-                    let fn_create_mirror_view_and_copy_htd_ident = format_ident!("create_mirror_view_and_copy_htd_{}", raw_extension);
-                    let fn_create_mirror_view_and_copy_dtd_ident = format_ident!("create_mirror_view_and_copy_dtd_{}", raw_extension);
+                    let fn_create_mirror_hth_ident =
+                        format_ident!("create_mirror_hth_{}", raw_extension);
+                    let fn_create_mirror_dth_ident =
+                        format_ident!("create_mirror_dth_{}", raw_extension);
+                    let fn_create_mirror_htd_ident =
+                        format_ident!("create_mirror_htd_{}", raw_extension);
+                    let fn_create_mirror_dtd_ident =
+                        format_ident!("create_mirror_dtd_{}", raw_extension);
+                    let fn_create_mirror_view_hth_ident =
+                        format_ident!("create_mirror_view_hth_{}", raw_extension);
+                    let fn_create_mirror_view_dth_ident =
+                        format_ident!("create_mirror_view_dth_{}", raw_extension);
+                    let fn_create_mirror_view_htd_ident =
+                        format_ident!("create_mirror_view_htd_{}", raw_extension);
+                    let fn_create_mirror_view_dtd_ident =
+                        format_ident!("create_mirror_view_dtd_{}", raw_extension);
+                    let fn_create_mirror_view_and_copy_hth_ident =
+                        format_ident!("create_mirror_view_and_copy_hth_{}", raw_extension);
+                    let fn_create_mirror_view_and_copy_dth_ident =
+                        format_ident!("create_mirror_view_and_copy_dth_{}", raw_extension);
+                    let fn_create_mirror_view_and_copy_htd_ident =
+                        format_ident!("create_mirror_view_and_copy_htd_{}", raw_extension);
+                    let fn_create_mirror_view_and_copy_dtd_ident =
+                        format_ident!("create_mirror_view_and_copy_dtd_{}", raw_extension);
 
                     let fn_get_at_args = (0..dim_val_usize)
                         .map(|i| format_ident!("i{i}"))
@@ -684,7 +696,7 @@ ViewHolder_{device_extension}* create_mirror_view_and_copy_dtd_{raw_extension}(c
                             },
                             _marker: PhantomData,
                         }
-                        
+
                     }
 
                 };
