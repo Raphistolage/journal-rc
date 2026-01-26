c614178984388f5eb4876d9f5f3608f69bb93a62
Raphistolage <--add>
2026-01-26 15:22:15 +0100

    Test de create_mirror_view_and_copy et extent

--- tests_macro/src/main.rs
+++ tests_macro/src/main.rs
@@ -10,21 +10,23 @@ fn main() {
             &[2, 3],
             &[1.0, 2.0, 3.0, 4.0, 5.0, 6.0],
         );
-        let mut v2 = ffi::View::<f64, ffi::Dim2, ffi::LayoutRight, ffi::HostSpace>::from_shape(
+        let v2 = ffi::View::<f64, ffi::Dim2, ffi::LayoutRight, ffi::HostSpace>::from_shape(
             &[2, 3],
-            &[2.0, 2.0, 3.0, 4.0, 5.0, 6.0],
+            &[0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
         );
+
         println!("Views created.");
 
-        let mut v3 = ffi::create_mirror(ffi::HostSpace(), &v1);
+        let  v3 = ffi::create_mirror_view_and_copy(ffi::HostSpace(), &v1);
 
-        println!("Mirror view created.");
-
-        ffi::deep_copy(&mut v3, &v1);
-
-        println!("Deep copy done.");
+        println!("Mirror view created and copy.");
 
         assert_eq!(v3[(0, 0)], 1.0);
+
+        let d1 = v3.extent(0).unwrap();
+        let d2 = v3.extent(1).unwrap();
+
+        println!("Extents of v3 : {d1}, {d2}");
     }
 
     ffi::kokkos_finalize();
